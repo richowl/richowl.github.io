e3775b61ce917a01cfbf95ad0bd3e027e2f365b6
Seungjae Park <astro.psj@gmail.com>
2022-06-16 19:20:33 +0900

    Replace feather icon to material symbol for reducing size

--- themes/archie2/static/js/themetoggle.js
+++ themes/archie2/static/js/themetoggle.js
@@ -2,11 +2,11 @@ function setTheme(mode) {
     localStorage.setItem("theme-storage", mode);
     if (mode === "dark") {
         document.getElementById("darkModeStyle").disabled=false;
-        document.getElementById("dark-mode-toggle").innerHTML = "<i data-feather=\"sun\"></i>";
+        document.getElementById("dark-mode-toggle").innerHTML = "<img class=\"icon\" src=\"/ic_light_mode.svg\">";
         feather.replace()
     } else if (mode === "light") {
         document.getElementById("darkModeStyle").disabled=true;
-        document.getElementById("dark-mode-toggle").innerHTML = "<i data-feather=\"moon\"></i>";
+        document.getElementById("dark-mode-toggle").innerHTML = "<img class=\"icon\" src=\"/ic_dark_mode.svg\">";
         feather.replace()
     }
 }
